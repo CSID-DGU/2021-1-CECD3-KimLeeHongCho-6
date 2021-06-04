e284eacb7eb341c44bc1287c64f6f1d77651b02e
soybean-dou <iu98@naver.com>
2021-06-04 19:50:51 +0900

    add subtitle model

--- public/index.js
+++ public/index.js
@@ -9,3 +9,16 @@ function load_video(){
     var video_content = document.getElementById("video_content");
     video_content.src = window.URL.createObjectURL(video_file);
 }
+
+function load_text(){
+    var fileObject = document.getElementById("text_file");
+    var fileName = fileObject.files[0];
+
+    var fr = new FileReader();
+    fr.readAsText(fileName, "utf-8");
+
+    fr.onload=()=>{
+        var text = document.getElementById("text_content");
+        text.innerText = fr.result;
+    }
+}
