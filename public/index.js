7c248be43937e5a6e6ccc2f5350eab11cf616433
soybean-dou <iu98@naver.com>
2021-06-04 18:10:14 +0900

    modify video add func

--- public/index.js
+++ public/index.js
@@ -3,3 +3,9 @@ var text=textract.fromFileWithPath("myfile.odt");
 
 var t = document.getElementById("textract");
 t.innerText = text;
+
+function load_video(){
+    var video_file = document.getElementById("video_file").files[0];
+    var video_content = document.getElementById("video_content");
+    video_content.src = window.URL.createObjectURL(video_file);
+}
