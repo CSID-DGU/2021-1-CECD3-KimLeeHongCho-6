0cae011ed9a1736b1ca2b209f0ea3f1c80d0a6f3
YoonhoKim655 <maumnamu2@naver.com>
2021-06-04 23:41:35 +0900

    Subtitle's functions and Overlay

--- public/index.js
+++ public/index.js
@@ -6,19 +6,6 @@ t.innerText = text;
 
 function load_video(){
     var video_file = document.getElementById("video_file").files[0];
-    var video_content = document.getElementById("video_content");
+    var video_content = document.getElementById("video-1");
     video_content.src = window.URL.createObjectURL(video_file);
-}
-
-function load_text(){
-    var fileObject = document.getElementById("text_file");
-    var fileName = fileObject.files[0];
-
-    var fr = new FileReader();
-    fr.readAsText(fileName, "utf-8");
-
-    fr.onload=()=>{
-        var text = document.getElementById("text_content");
-        text.innerText = fr.result;
-    }
 }
