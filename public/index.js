db4090881a0cf259ac4e409218f62d7b0ccd7d6f
YoonhoKim655 <maumnamu2@naver.com>
2021-06-11 17:11:00 +0900

    Add Subtitle button

--- public/index.js
+++ public/index.js
@@ -9,3 +9,9 @@ function load_video(){
     var video_content = document.getElementById("video-1");
     video_content.src = window.URL.createObjectURL(video_file);
 }
+
+function load_Subtitle(){
+    var subtitle_file = document.getElementById("subtitle_file").files[0];
+    var subtitle_content = document.getElementById("entrack-1");
+    subtitle_content.src = window.URL.createObjectURL(subtitle_file);
+}
