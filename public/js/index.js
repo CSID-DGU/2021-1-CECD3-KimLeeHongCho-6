7dce398ec70935225b18c7e5c80bdc6ebda3f700
YoonhoKim655 <maumnamu2@naver.com>
2021-10-03 23:35:54 +0900

    Subtitle tabs and User Subtitle

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+var sub_num = 1;
 var video_bnt = document.getElementById('video_file');
 video_bnt.addEventListener('change', function (){
     var video_file = video_bnt.files[0];
@@ -7,9 +8,23 @@ video_bnt.addEventListener('change', function (){
 
 var subtitle_bnt = document.getElementById('subtitle_file')
 subtitle_bnt.addEventListener('change',function(){
-    var subtitle_file = subtitle_bnt.files[0];
-    var subtitle_content = document.getElementById("entrack-1");
-    subtitle_content.src = window.URL.createObjectURL(subtitle_file);
+        var subtitle_file = subtitle_bnt.files[0];
+        var subtitle_content = document.getElementById("entrack-"+sub_num);
+        subtitle_content.src = window.URL.createObjectURL(subtitle_file);
+
+        sub_num++;
+        var addStaffText = '<track onload="onTrackLoad('+sub_num+');" class="subtitle_content" id="entrack-'+sub_num+'" label="English" kind="subtitles" srclang="ko">';
+        var trHtml = $( "track:last" );
+        trHtml.after(addStaffText);
+    if(sub_num >= 4){
+        var addTab = '<li class="tab-link" data-tab="tab-'+sub_num+'">자막_'+sub_num+'</li>';
+        var tabHtml = $( "li[class=tab-link]:last" );
+        tabHtml.after(addTab);
+
+        var addTabCon = '<div id="tab-'+sub_num+'" class="tab-content"><div id="display-cues-'+sub_num+'"><p id="start-point-'+sub_num+'"></p></div></div>';
+        var tabConHtml = $( "div[class=tab-content]:last" );
+        tabConHtml.after(addTabCon);
+    }
 })
 
 $(document).ready(function () {
@@ -25,11 +40,11 @@ $(document).ready(function () {
             if (i == 0) {
                 cuesTime[video_id] = [];
                 cuesTime[video_id][i] = 0;
-                $('#display-cues-' + video_id).append('<p onclick="jumpToTime(' + video_id + ', ' + cues[i].startTime + ')" data-start-time-video-' + video_id + '="0" class="cue-active">' + cues[i].text + '</p>');
+                $('#display-cues-' + video_id).append('<p onclick="jumpToTime(' + 1 + ', ' + cues[i].startTime + ')" data-start-time-video-' + video_id + '="0" class="cue-active">' + cues[i].text + '</p>');
             }
             else {
                 cuesTime[video_id][i] = cues[i].startTime;
-                $('#display-cues-' + video_id).append('<p onclick="jumpToTime(' + video_id + ', ' + cues[i].startTime + ')" data-start-time-video-' + video_id + '="' + cues[i].startTime + '">' + cues[i].text + '</p>');
+                $('#display-cues-' + video_id).append('<p onclick="jumpToTime(' + 1 + ', ' + cues[i].startTime + ')" data-start-time-video-' + video_id + '="' + cues[i].startTime + '">' + cues[i].text + '</p>');
             }
         }
 
@@ -52,6 +67,7 @@ $(document).ready(function () {
         else{
           for (var i = 0; i < cues.length; i++) {
           if(cues[i].text.includes(search)){
+            count++;
             var subt = cues[i].text;
             var regex = new RegExp(search, "g");
             subt = subt.replace(regex, "<span class='highlight'>" + search + "</span>"); // highlight class
@@ -59,12 +75,10 @@ $(document).ready(function () {
             if(i == 0){
               cuesTime[video_id] = [];
               cuesTime[video_id][i] = 0;
-               $('#s_area').append('<p class="s_text" onclick="jumpToTime(' + video_id + ', ' + cues[i].startTime + ')" sub-data-start-time-video-' + video_id + '="0">' + subt + '</p>');
-               count++;				
+               $('#s_area').append('<p class="s_text" onclick="jumpToTime(' + video_id + ', ' + cues[i].startTime + ')" sub-data-start-time-video-' + video_id + '="0">' + subt + '</p>');				
             }else{
               cuesTime[video_id][i] = cues[i].startTime;
               $('#s_area').append('<p class="s_text" onclick="jumpToTime(' + video_id + ', ' + cues[i].startTime + ')" sub-data-start-time-video-' + video_id + '="' + cues[i].startTime + '">' + subt + '</p>');
-              count++;
             }
           }
         }
@@ -84,7 +98,7 @@ $(document).ready(function () {
                 //console.log(video_id);
                 var currentTime = this.currentTime;
 
-
+                for(video_id = 1; video_id < sub_num; video_id++){
                 var cueTimeCurrent = 0;
                 for (var i = 0; i < cuesTime[video_id].length; i++) {
                     if ((currentTime - cuesTime[video_id][i]) <= 0) {
@@ -97,15 +111,15 @@ $(document).ready(function () {
                 if (currentTime == 0) {
                     cueTimeCurrent = 0;
                 }
-
+                //for(var i = i; i <= sub_num; i++){
                 $('#display-cues-' + video_id + ' .cue-active').removeClass('cue-active');
                 $('[data-start-time-video-' + video_id + '="' + cueTimeCurrent + '"]').addClass('cue-active');
-
+                //}
                 var offsetTop = $('[data-start-time-video-' + video_id + '="' + cueTimeCurrent + '"]').offset().top - $('#start-point-' + video_id).offset().top;
                 if (!onHover[video_id]) {
                     $('#display-cues-' + video_id).scrollTop(offsetTop - 150);
                 }
-                //console.log(offsetTop);
+                }
             }
         );
 
@@ -120,6 +134,16 @@ $(document).ready(function () {
                 onHover[video_id] = false;
             }
         );
+
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+    
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+    
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
 
 
     $("#upload_file").on('change', function () {
@@ -159,6 +183,12 @@ $('#toPlay_btn').hover(function(){
 });
 
 $('#toEdit_btn').hover(function(){
+    $(this).children('img').css("filter","invert(97%) sepia(0%) saturate(0%) hue-rotate(60deg) brightness(103%) contrast(103%)");
+},function(){
+    $(this).children('img').css("filter", "invert(39%) sepia(62%) saturate(270%) hue-rotate(190deg) brightness(89%) contrast(90%)");
+});
+
+$('#toUser_btn').hover(function(){
     $(this).children('img').css("filter","invert(97%) sepia(0%) saturate(0%) hue-rotate(60deg) brightness(103%) contrast(103%)");
 },function(){
     $(this).children('img').css("filter", "invert(39%) sepia(62%) saturate(270%) hue-rotate(190deg) brightness(89%) contrast(90%)");
